ad4db12b79fbef588a6f826a03475d2c2cb9b5f4
maybeSwarup <suryawanshiswarup@gmail.com>
2025-09-06 13:05:54 +0530

    test: Add failing test for single number

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,3 +3,7 @@ import { Add } from "../src/calculator.js";
 test("returns 0 for empty string", () => {
   expect(Add("")).toBe(0);
 });
+
+test("returns the number for single number", () => {
+  expect(Add("1")).toBe(1);
+});
