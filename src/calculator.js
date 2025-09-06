9a13b166a22f6f936518149d6e26c7dd1f5b54e4
maybeSwarup <suryawanshiswarup@gmail.com>
2025-09-06 13:16:14 +0530

    feat: Support custom delimiter

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,13 @@
 export function Add(numbers) {
   if (numbers === "") return 0;
+  
+  if (numbers.startsWith("//")) {
+    const delimiter = numbers[2];
+    const numbersPart = numbers.substring(4);
+    const nums = numbersPart.split(delimiter);
+    return nums.reduce((sum, num) => sum + parseInt(num), 0);
+  }
+  
   if (numbers.includes(",") || numbers.includes("\n")) {
     const nums = numbers.split(/[,\n]/);
     return nums.reduce((sum, num) => sum + parseInt(num), 0);
