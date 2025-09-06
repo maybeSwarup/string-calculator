1b5d424ce40da14340285ee9db935314e1e243b3
maybeSwarup <suryawanshiswarup@gmail.com>
2025-09-06 13:08:38 +0530

    feat: Handle two numbers case

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,8 @@
 export function Add(numbers) {
   if (numbers === "") return 0;
+  if (numbers.includes(",")) {
+    const nums = numbers.split(",");
+    return parseInt(nums[0]) + parseInt(nums[1]);
+  }
   return parseInt(numbers);
 }
