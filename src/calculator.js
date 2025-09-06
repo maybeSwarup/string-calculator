7aceb7cd4d6fbb13a0fa6ade26ef6284bcde4e5d
maybeSwarup <suryawanshiswarup@gmail.com>
2025-09-06 13:12:49 +0530

    feat: Support newline as delimiter

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,7 @@
 export function Add(numbers) {
   if (numbers === "") return 0;
-  if (numbers.includes(",")) {
-    const nums = numbers.split(",");
+  if (numbers.includes(",") || numbers.includes("\n")) {
+    const nums = numbers.split(/[,\n]/);
     return nums.reduce((sum, num) => sum + parseInt(num), 0);
   }
   return parseInt(numbers);
